b62b37bed5b28b3f93a58e50f9e29f7b3a41a6bb
xusenlin <xusenlin@dnect.cn>
2023-09-15 11:26:31 +0800

    Support for chinese-llama-alpaca-2

--- api/apapter/conversation.py
+++ api/apapter/conversation.py
@@ -517,7 +517,13 @@ class Llama2ChatPromptAdapter(BasePromptAdapter):
     """
 
     name = "llama2"
-    system_prompt = "[INST] <<SYS>>\n{}\n<</SYS>>\n\n"
+    system_prompt = """[INST] <<SYS>>
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+
+"""
     user_prompt = "[INST] {} "
     assistant_prompt = "[/INST] {} </s><s>"
     stop = {
@@ -528,10 +534,7 @@ class Llama2ChatPromptAdapter(BasePromptAdapter):
         return "llama2" in model_name or "code-llama" in model_name
 
     def generate_prompt(self, messages: List[ChatMessage]) -> str:
-        prompt = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
-        prompt = self.system_prompt.format(prompt)
+        prompt = self.system_prompt
         for i, message in enumerate(messages):
             role, content = message.role, message.content
             if i == 0:
@@ -546,6 +549,20 @@ If a question does not make any sense, or is not factually coherent, explain why
             prompt += "[/INST] "
 
         return prompt
+
+
+class ChineseAlpaca2ChatPromptAdapter(Llama2ChatPromptAdapter):
+    """ https://github.com/ymcui/Chinese-LLaMA-Alpaca-2 """
+
+    name = "chinese-llama-alpaca2"
+    system_prompt = """[INST] <<SYS>>
+You are a helpful assistant. 你是一个乐于助人的助手。
+<</SYS>>
+
+"""
+
+    def match(self, model_name) -> bool:
+        return "chinese-llama-alpaca-2" in model_name
 
 
 class QwenPromptAdapter(BasePromptAdapter):
@@ -633,6 +650,7 @@ register_prompt_adapter(BaiChuan2PromptAdapter)
 register_prompt_adapter(StarChatPromptAdapter)
 register_prompt_adapter(AquilaChatPromptAdapter)
 register_prompt_adapter(Llama2ChatPromptAdapter)
+register_prompt_adapter(ChineseAlpaca2ChatPromptAdapter)
 register_prompt_adapter(QwenPromptAdapter)
 register_prompt_adapter(OctopackPromptAdapter)
 register_prompt_adapter(XversePromptAdapter)
