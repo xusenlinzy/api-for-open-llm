83018264c838b9dece395495a395770e15bed360
xusenlin <xusenlin@dnect.cn>
2023-10-29 10:30:33 +0800

    Update chatglm3 tool using examples

--- examples/chatglm3/tool_using.py
+++ examples/chatglm3/tool_using.py
@@ -1,50 +1,19 @@
 import json
 
 import openai
+from loguru import logger
+
+from tool_register import get_tools, dispatch_tool
 
 openai.api_base = "http://192.168.20.59:7891/v1"
 openai.api_key = "xxx"
 
 
-tools = [
-    {
-        "name": "track",
-        "description": "追踪指定股票的实时价格",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "symbol": {
-                    "description": "需要追踪的股票代码"
-                }
-            },
-            "required": ['symbol']
-        }
-    },
-    {
-        "name": "text-to-speech",
-        "description": "将文本转换为语音",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "text": {
-                    "description": "需要转换成语音的文本"
-                },
-                "voice": {
-                    "description": "要使用的语音类型（男声、女声等）"
-                },
-                "speed": {
-                    "description": "语音的速度（快、中等、慢等）"
-                }
-            },
-            "required": ['text']
-        }
-    }
-]
-
+tools = get_tools()
 system_info = {
     "role": "system",
     "content": "Answer the following questions as best as you can. You have access to the following tools:",
-    "tools": tools,
+    "tools": list(tools.values()),
 }
 
 
@@ -53,7 +22,7 @@ def test():
         system_info,
         {
             "role": "user",
-            "content": "帮我查询股票10111的价格",
+            "content": "帮我查询北京的天气怎么样",
         }
     ]
     response = openai.ChatCompletion.create(
@@ -61,13 +30,17 @@ def test():
         messages=messages,
         temperature=0,
     )
-    print(response.choices[0].message.content)
+    function_call = json.loads(response.choices[0].message.content)
+    logger.info(f"Function Call Response: {function_call}")
+
+    tool_response = dispatch_tool(function_call["name"], function_call["parameters"])
+    logger.info(f"Tool Call Response: {tool_response}")
 
     messages = response.choices[0].history  # 获取历史对话信息
     messages.append(
         {
             "role": "observation",
-            "content": json.dumps({"price": 12412}, ensure_ascii=False),  # 调用函数返回结果
+            "content": tool_response,  # 调用函数返回结果
         }
     )
 
@@ -76,7 +49,7 @@ def test():
         messages=messages,
         temperature=0,
     )
-    print(response.choices[0].message.content)
+    logger.info(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
